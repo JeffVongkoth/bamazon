be843899a65f3f1b028122a0b6f8c87a9368ebd9
Jeff Vongkoth <jvongkoth@gmail.com>
2018-06-13 21:49:41 -0500

    started on checking product function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,56 +1,86 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-nodemon()
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'jeff13579',
-  database: 'bamazon'
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'jeff13579',
+    database: 'bamazon'
 }); //info to connect to database
 
-var myCallback = function(err, response) {
-  // handle the err and response
-  if (err) throw (err);
+let sqlCommand = 'SELECT * FROM products'; //accessing the table 'products'
+
+
+function queryProducts() {
+    connection.query(sqlCommand, function (err, res) {
+        console.log("\n-----------------------------------");
+        for (let i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | ");
+        }
+        console.log("/n-----------------------------------");
+    });
 }
 
-var sqlCommand = 'SELECT * FROM products'; //accessing the table 'products'
+function checkProduct() {
+    connection.query(sqlCommand, function (err, res) {
+        for (let i = 0; i < res.length; i++) {
+            
+            let stock = res[i].stock_quantity;
+            console.log(chosenQuanitity,stock);
+if (chosenQuanitity <= stock_quantity) {
 
-// connection.connect(function(err) {
-//     if (err) {console.log('connection.js ', err);}
-//     console.log('connected! ', connection.threadId);
-
-//     connection.query(sqlCommand, function (err,res){
-//     if (err) {console.log('connection.js ', err);}
-//     // console.log('response ', response);
-//     });
-// })
-
-function showProduct() {
-  connection.query(sqlCommand, myCallback);
-  console.log(myCallback);
 }
-showProduct()
+else if (stock <= 0 ) {
+    console.log("-----------------------------------");
+    console.log('Sorry not enough shit for you order');
+}
+        }
+
+    })
+}
+// checkProduct();
+function productPrompt() {
+    queryProducts();
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Which product would you like?",
+                choices: ['stroller', 'sword', 'car wash soap', 'toilet paper', 'AA battery', 'white t-shirt',
+                    'pencils', 'tooth brush', 'RC car', 'Tablet'],
+                name: "product"
+            },
+            {
+                type: 'input',
+                message: 'How much of this product would you like?',
+                name: 'quantity'
+            }
+        ])
+        .then(function (inquirerResponse) {
+checkProduct();
+        })
+}
 
 
-// inquirer
-//   .prompt([
-// {
-//   type: "list",
-//   message: "Which product would you like?",
-//   choices: ['stroller', 'sword', 'car wash soap', 'toilet paper', 'AA battery', 'white t-shirt',
-//   'pencils', 'tooth brush', 'RC car', 'Tablet'],
-//   name: "product"
-// },
-// {
-// type:'input',
-// message:'How much of this product would you like?',
-// name:'quantity'
-// }
-//   ])
-// .then(function(product) {
-  
-// })
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+
+function afterConnection() {
+    connection.query(sqlCommand, function (err, res) {
+        if (err) throw err;
+        // checkProduct();
+
+        //   console.log(res);
+        productPrompt();
+        connection.end();
+    });
+}
+
+
 
