b88dfff68404e719cb4071f4d63a0ddbc6ddcdf7
Jeff Vongkoth <jvongkoth@gmail.com>
2018-06-14 22:56:45 -0500

    fixed prompting issues and added screenshots

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,34 +15,45 @@ let sqlCommand = 'SELECT * FROM products'; //accessing the table 'products'
 
 function queryProducts() {
     connection.query(sqlCommand, function (err, res) {
+        if (err) throw err;
         console.log("\n-----------------------------------");
         for (let i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | ");
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + '$', res[i].price + " | ");
         }
-        console.log("/n-----------------------------------");
-    });
+        console.log("\n-----------------------------------");
+        showQuestions();
+    })
+    // showQuestions();
 }
 
-function checkProduct() {
+function checkProduct(selection) {
     connection.query(sqlCommand, function (err, res) {
+        if (err) throw err;
+        console.log(selection);
+        let chosenProduct = selection.product;
+        console.log(chosenProduct);
+        let chosenQuantity = selection.quantity;
+        console.log(chosenQuantity);
+        
         for (let i = 0; i < res.length; i++) {
-            
+            // console.log(res);
             let stock = res[i].stock_quantity;
-            console.log(chosenQuanitity,stock);
-if (chosenQuanitity <= stock_quantity) {
+            console.log(stock);
 
-}
-else if (stock <= 0 ) {
-    console.log("-----------------------------------");
-    console.log('Sorry not enough shit for you order');
-}
+            // // chosenQuanitity, 
+            // if (selection <= stock_quantity) {
+            //     console.log('Your transaction has been completed');
+            // }
+            // else if (stock <= 0) {
+            //     console.log("-----------------------------------");
+            //     console.log('Sorry not enough shit for you order');
+            // }
         }
 
     })
 }
-// checkProduct();
-function productPrompt() {
-    queryProducts();
+
+function showQuestions() {
     inquirer
         .prompt([
             {
@@ -59,7 +70,8 @@ function productPrompt() {
             }
         ])
         .then(function (inquirerResponse) {
-checkProduct();
+        
+            checkProduct(inquirerResponse);
         })
 }
 
@@ -68,19 +80,13 @@ checkProduct();
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    queryProducts();
     afterConnection();
 });
 
 function afterConnection() {
     connection.query(sqlCommand, function (err, res) {
         if (err) throw err;
-        // checkProduct();
-
-        //   console.log(res);
-        productPrompt();
-        connection.end();
+        // connection.end();
     });
 }
-
-
-
